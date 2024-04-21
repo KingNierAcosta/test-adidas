eae7236b4fe47dbf1f4dfe53d10c0f2154138016
KingNier <kingnieracosta@gmail.com>
2024-04-21 00:51:49 -0600

    product list layout

--- src/app/components/auxiliary-menu/auxiliary-menu.component.ts
+++ src/app/components/auxiliary-menu/auxiliary-menu.component.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { InputSearchComponent } from "../input-search/input-search.component";
+import { BtnIconFavoriteComponent } from "../btn-icon-favorite/btn-icon-favorite.component";
 
 @Component({
-    selector: 'app-auxiliary-menu',
-    standalone: true,
-    templateUrl: './auxiliary-menu.component.html',
-    styleUrl: './auxiliary-menu.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [
-        CommonModule,
-        InputSearchComponent
-    ]
+  selector: 'app-auxiliary-menu',
+  standalone: true,
+  templateUrl: './auxiliary-menu.component.html',
+  styleUrl: './auxiliary-menu.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    InputSearchComponent,
+    BtnIconFavoriteComponent
+  ]
 })
 export class AuxiliaryMenuComponent {
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ProductItemComponent } from "../../components/product-item/product-item.component";
 
 @Component({
-  selector: 'app-products-list',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './products-list.component.html',
-  styles: `
+    selector: 'app-products-list',
+    standalone: true,
+    templateUrl: './products-list.component.html',
+    styles: `
     :host {
       display: block;
     }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        ProductItemComponent
+    ]
 })
 export class ProductsListComponent { }
